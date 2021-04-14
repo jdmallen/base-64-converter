f1347b4a73acbdcc5f805b5928a132325574fc4f
J.D. Mallen <lightandtheheat@gmail.com>
2021-04-14 12:08:22 -0400

    Initial commit, not broken yet

---
+++ JDMallen.Base64Converter/Base64Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JDMallen.Base64Converter
+{
+	public partial class Base64Converter : Form
+	{
+		public Base64Converter()
+		{
+			InitializeComponent();
+		}
+	}
+}
