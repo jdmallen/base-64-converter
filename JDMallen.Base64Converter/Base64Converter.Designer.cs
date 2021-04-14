54cd8f76cf0cbbf81321fe04a6597d3c4a4a716d
J.D. Mallen <lightandtheheat@gmail.com>
2021-04-14 12:12:00 -0400

    Got it growing

--- JDMallen.Base64Converter/Base64Converter.Designer.cs
+++ JDMallen.Base64Converter/Base64Converter.Designer.cs
@@ -30,16 +30,21 @@ namespace JDMallen.Base64Converter
 		private void InitializeComponent()
 		{
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.input = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel1
 			// 
+			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.AutoSize = true;
+			this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.tableLayoutPanel1.ColumnCount = 1;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.input, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -49,16 +54,27 @@ namespace JDMallen.Base64Converter
 			this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 426);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
-			// textBox1
+			// input
 			// 
-			this.textBox1.Location = new System.Drawing.Point(3, 3);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 23);
-			this.textBox1.TabIndex = 0;
+			this.input.AcceptsReturn = true;
+			this.input.AcceptsTab = true;
+			this.input.AllowDrop = true;
+			this.input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.input.HideSelection = false;
+			this.input.Location = new System.Drawing.Point(3, 3);
+			this.input.MaxLength = 1073741824;
+			this.input.Multiline = true;
+			this.input.Name = "input";
+			this.input.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.input.Size = new System.Drawing.Size(770, 390);
+			this.input.TabIndex = 0;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(3, 399);
+			this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.button1.Location = new System.Drawing.Point(350, 399);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 1;
@@ -76,13 +92,14 @@ namespace JDMallen.Base64Converter
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tableLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox input;
 		private System.Windows.Forms.Button button1;
 	}
 }
